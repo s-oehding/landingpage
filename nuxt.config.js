efab2088ccc9943640aad1756f19398aa5ccf484
Sören Oehding <oehding@werk4.net>
2018-08-06 12:54:17 +0200

    quickfix missing icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,8 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,900' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700,900' },
+      { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.2.0/css/all.css', integrity: 'sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ', crossOrigin: 'anonymus' }
     ]
   },
   /*
